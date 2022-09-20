7b8ddfa9f1dd2675ee7fe8b92b24abe1aaa3d5f8
Marlon Miller <mmillermatute18@gmail.com>
2022-09-19 23:57:27 -0500

    Textviews and words now in second activity. About to write story as last step.

--- app/src/main/java/com/example/my_craze_mad_lip_mm/DisplayMessageActivity.java
+++ app/src/main/java/com/example/my_craze_mad_lip_mm/DisplayMessageActivity.java
@@ -13,10 +13,55 @@ public class DisplayMessageActivity extends AppCompatActivity {
         setContentView(R.layout.activity_display_message);
 
         Intent intent = getIntent();
-        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
-
-        // Capture the layout's TextView and set the string as its text
+        String word1 = intent.getStringExtra("First_Word");
         TextView textView = findViewById(R.id.textView);
-        textView.setText(message);
+        textView.setText(word1);
+
+        Intent intent2 = getIntent();
+        String word2 = intent2.getStringExtra("Second_Word");
+        TextView textView2 = findViewById(R.id.textView2);
+        textView2.setText(word2);
+
+        Intent intent3 = getIntent();
+        String word3 = intent3.getStringExtra("Third_Word");
+        TextView textView3 = findViewById(R.id.textView3);
+        textView3.setText(word3);
+
+        Intent intent4 = getIntent();
+        String word4 = intent4.getStringExtra("Fourth_Word");
+        TextView textView4 = findViewById(R.id.textView4);
+        textView4.setText(word4);
+
+        Intent intent5 = getIntent();
+        String word5 = intent5.getStringExtra("Fifth_Word");
+        TextView textView5 = findViewById(R.id.textView5);
+        textView5.setText(word5);
+
+        Intent intent6 = getIntent();
+        String word6 = intent6.getStringExtra("Sixth_Word");
+        TextView textView6 = findViewById(R.id.textView6);
+        textView6.setText(word6);
+
+        Intent intent7 = getIntent();
+        String word7 = intent7.getStringExtra("Seventh_Word");
+        TextView textView7 = findViewById(R.id.textView7);
+        textView7.setText(word7);
+
+        Intent intent8 = getIntent();
+        String word8 = intent8.getStringExtra("Eighth_Word");
+        TextView textView8 = findViewById(R.id.textView8);
+        textView8.setText(word8);
+
+        Intent intent9 = getIntent();
+        String word9 = intent9.getStringExtra("Ninth_Word");
+        TextView textView9 = findViewById(R.id.textView9);
+        textView9.setText(word9);
+
+        Intent intent10 = getIntent();
+        String word10 = intent10.getStringExtra("Tenth_Word");
+        TextView textView10 = findViewById(R.id.textView10);
+        textView10.setText(word10);
     }
 }
+
+// Capture the layout's TextView and set the string as its text
--- app/src/main/java/com/example/my_craze_mad_lip_mm/MainActivity.java
+++ app/src/main/java/com/example/my_craze_mad_lip_mm/MainActivity.java
@@ -19,8 +19,39 @@ public class MainActivity extends AppCompatActivity {
     public void sendMessage(View view) {
         Intent intent = new Intent(this, DisplayMessageActivity.class);
         EditText editText = (EditText) findViewById(R.id.First_Word);
+        EditText editText2 = (EditText) findViewById(R.id.Second_Word);
+        EditText editText3 = (EditText) findViewById(R.id.Third_Word);
+        EditText editText4 = (EditText) findViewById(R.id.Fourth_Word);
+        EditText editText5 = (EditText) findViewById(R.id.Fifth_Word);
+        EditText editText6 = (EditText) findViewById(R.id.Sixth_Word);
+        EditText editText7 = (EditText) findViewById(R.id.Seventh_Word);
+        EditText editText8 = (EditText) findViewById(R.id.Eighth_Word);
+        EditText editText9 = (EditText) findViewById(R.id.Ninth_Word);
+        EditText editText10 = (EditText) findViewById(R.id.Tenth_Word);
+
         String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
+        String message2 = editText2.getText().toString();
+        String message3 = editText3.getText().toString();
+        String message4 = editText4.getText().toString();
+        String message5 = editText5.getText().toString();
+        String message6 = editText6.getText().toString();
+        String message7 = editText7.getText().toString();
+        String message8 = editText8.getText().toString();
+        String message9 = editText9.getText().toString();
+        String message10 = editText10.getText().toString();
+
+
+        intent.putExtra("First_Word", message);
+        intent.putExtra("Second_Word", message2);
+        intent.putExtra("Third_Word", message3);
+        intent.putExtra("Fourth_Word", message4);
+        intent.putExtra("Fifth_Word", message5);
+        intent.putExtra("Sixth_Word", message6);
+        intent.putExtra("Seventh_Word", message7);
+        intent.putExtra("Eighth_Word", message8);
+        intent.putExtra("Ninth_Word", message9);
+        intent.putExtra("Tenth_Word", message10);
+
         startActivity(intent);
     }
 }
